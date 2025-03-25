3dfaea149269d0bb9a314cc0ad15fbd1fc652353
Yevhen <yevhen.yatskiv@gmail.com>
2025-03-25 17:09:22 +0100

    Added my personal logo

--- simple-app/src/App.tsx
+++ simple-app/src/App.tsx
@@ -4,9 +4,10 @@ import './App.css'
 import arrow from './assets/arrow.png'
 import lynxLogo from './assets/lynx-logo.png'
 import reactLynxLogo from './assets/react-logo.png'
+import yyLogo from './assets/yy-logo.png'
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false)
+  const [alterLogo, setAlterLogo] = useState(0)
 
   useEffect(() => {
     console.info('Hello, ReactLynx')
@@ -14,8 +15,11 @@ export function App() {
 
   const onTap = useCallback(() => {
     'background only'
-    setAlterLogo(!alterLogo)
-  }, [alterLogo])
+    setAlterLogo((prevLogo) => {
+      const newLogo = (prevLogo + 1) % 3;
+      return newLogo;
+    });
+  }, [alterLogo]);
 
   return (
     <view>
@@ -23,22 +27,19 @@ export function App() {
       <view className='App'>
         <view className='Banner'>
           <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
+            {alterLogo === 0 && <image src={yyLogo} className='Logo--yy' />}
+            {alterLogo === 1 && <image src={lynxLogo} className='Logo--lynx' />}
+            {alterLogo === 2 && <image src={reactLynxLogo} className='Logo--react' />}
           </view>
           <text className='Title'>React</text>
           <text className='Subtitle'>on Lynx</text>
+          <text className='Version'>0.0.1</text>
         </view>
         <view className='Content'>
-          <image src={arrow} className='Arrow' />
+          <image src={arrow} className='Arrow'/>
           <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
-            to see updates!
-          </text>
+          <text className='Hint'>Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>to see updates!</text>
         </view>
-        <view style={{ flex: 1 }}></view>
       </view>
     </view>
   )
